62414c797c468d64dbf074a93a4014575b1516c2
Artiom Lezcano Koretski <koretskiartiom@gmail.com>
2025-03-11 22:02:23 +0000

    rm post-command w ref to mh repos

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -9,9 +9,3 @@ curl -fsSL https://aka.ms/install-azd.sh | bash
 # Install Azure Functions Core Tools
 sudo apt-get update
 sudo apt-get install azure-functions-core-tools-4
-
-# Make all scripts in the scripts folder executable
-chmod +x ./scripts/*
-chmod +x ./code/gpt-rag-ingestion/scripts/*
-chmod +x ./code/gpt-rag-frontend/deploy.sh
-chmod +x ./code/gpt-rag-frontend/deploy.ps1
