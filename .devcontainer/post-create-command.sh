7822f03a87e2c865cbfd7abffea8fde9a2d64ba5
Artiom Lezcano Koretski <koretskiartiom@gmail.com>
2025-06-19 02:56:50 +0000

    install azure cli by default

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,7 +1,7 @@
-# #!/bin/bash
+#!/bin/bash
 
-# # Install Azure CLI
-# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# Install Azure CLI
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 # # Install Azure Developer CLI
 # curl -fsSL https://aka.ms/install-azd.sh | bash
