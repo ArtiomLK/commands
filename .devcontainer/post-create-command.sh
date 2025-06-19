c9b539832504fbf9483cde07aad12eae6020045e
Artiom Lezcano Koretski <koretskiartiom@gmail.com>
2025-06-18 22:40:32 -0500

    codespaces templates (#58)

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -3,9 +3,9 @@
 # Install Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-# Install Azure Developer CLI
-curl -fsSL https://aka.ms/install-azd.sh | bash
+# # Install Azure Developer CLI
+# curl -fsSL https://aka.ms/install-azd.sh | bash
 
-# Install Azure Functions Core Tools
-sudo apt-get update
-sudo apt-get install azure-functions-core-tools-4
+# # Install Azure Functions Core Tools
+# sudo apt-get update
+# sudo apt-get install azure-functions-core-tools-4
